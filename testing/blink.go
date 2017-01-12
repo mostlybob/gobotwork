8d442d5a0ca9588712ccd91226cd1c36621a00f2
Bob C <mostlybob@gmail.com>
2017-01-12 05:52:40 -0600

    playing with the interval

--- testing/blink.go
+++ testing/blink.go
@@ -13,7 +13,7 @@ func main() {
 	led := gpio.NewLedDriver(firmataAdaptor, "13")
 
 	work := func() {
-		gobot.Every(3*time.Second, func() {
+		gobot.Every(1*time.Second, func() {
 			led.Toggle()
 		})
 	}
